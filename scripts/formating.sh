70723c9df8f80cea830911ed626e5696e466f932
KodKio <stepanovnic.03@yandex.ru>
2022-03-17 22:57:44 +0300

    add clang-format to ci

---
+++ scripts/formating.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+TMP_DIR="./tmp"
+LOG_FILE="./tmp/clang-formating.log"
+TMP_FORM_FILE="./tmp/formated-file"
+mkdir -p $TMP_DIR
+touch $LOG_FILE
+touch $TMP_FORM_FILE
+
+STYLE="Google"
+FORMATING_FILES=("./main.c" "./exchange_rates.c" \
+                 "./exchange_rates.h" "./array_of_rates.h" \
+                 "./array_of_rates.c" "./tests.cpp")
+for file in ${FORMATING_FILES[*]}
+do
+    echo $file
+    clang-format -style=$STYLE $file > $TMP_FORM_FILE || (echo "Error, check $LOG_FILE")
+    diff $file $TMP_FORM_FILE
+done
