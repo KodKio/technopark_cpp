4b1d41ef2df8203dae53e9acbd28f739f0051534
KodKio <stepanovnic.03@yandex.ru>
2022-03-18 08:05:24 +0300

    add scan-build

--- scripts/formating.sh
+++ scripts/formating.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-
-TMP_DIR="./tmp"
-LOG_FILE="./tmp/clang-formating.log"
-TMP_FORM_FILE="./tmp/formated-file"
-mkdir -p $TMP_DIR
-touch $LOG_FILE
-touch $TMP_FORM_FILE
-
 STYLE="Google"
 FORMATING_FILES=("./main.c" "./exchange_rates.c" \
                  "./exchange_rates.h" "./array_of_rates.h" \
@@ -15,5 +7,5 @@ FORMATING_FILES=("./main.c" "./exchange_rates.c" \
 for file in ${FORMATING_FILES[*]}
 do
     echo $file
-    clang-format -i -style=$STYLE $file > $TMP_FORM_FILE || (echo "Error, check $LOG_FILE")
+    clang-format -i -style=$STYLE $file
 done
