cd7935742ab4661502f387bcfc95371dcc1f409e
KodKio <stepanovnic.03@yandex.ru>
2022-03-18 22:57:39 +0300

    fix ci

--- scripts/formating.sh
+++ scripts/formating.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 STYLE="Google"
-FORMATING_FILES=("./main.c" "./project/source/exchange_rate.c" \
+FILES=("./main.c" "./project/source/exchange_rate.c" \
                  "./project/include/exchange_rate.h" "./project/include/array_of_rates.h" \
                  "./project/source/array_of_rates.c" "./tests/tests.cpp")
 
-TMP_DIR="./tmp"
-LOG_FILE="./tmp/clang-formating.log"
-TMP_FORM_FILE="./tmp/formated-file"
-mkdir -p $TMP_DIR
-touch $LOG_FILE
-touch $TMP_FORM_FILE
+TMP_FILE="./tmp/formated-file"
+touch $TMP_FILE
 
 for file in ${FORMATING_FILES[*]}
 do
     echo $file
-    clang-format -style=$STYLE $file > $TMP_FORM_FILE
+    clang-format -style=$STYLE $file  > $TMP_FILE
     diff $file $TMP_FORM_FILE
 done
