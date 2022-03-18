142d97ab968112fcc39243b784b8605d6fe71a9b
KodKio <stepanovnic.03@yandex.ru>
2022-03-18 09:11:53 +0300

    add fbiner to ci

---
+++ scripts/install_fbiner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILE=/opt/infer-linux64-v$VERSION/bin/infer
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+  VERSION=1.0.0; \
+  curl -sSL "https://github.com/facebook/infer/releases/download/v$VERSION/infer-linux64-v$VERSION.tar.xz" \
+  | sudo tar -C /opt -xJ && \
+  sudo ln -s "/opt/infer-linux64-v$VERSION/bin/infer" /usr/local/bin/infer
+fi
